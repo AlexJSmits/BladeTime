0fee9b4efcfe1bb410a82dd5ce6a2406f67d4db8
Alex Smits <alexsmits247@gmail.com>
2021-04-14 17:49:13 +1000

    Basic Movement with touch controls

---
+++ BladeTimeUnity/Assets/Scripts/Movement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.UIElements;
+
+public class Movement : MonoBehaviour
+{
+
+    public LayerMask whatCanBePressedOn;
+
+    private NavMeshAgent myAgent;
+
+    void Start()
+    {
+        myAgent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            
+            Ray myRay = Camera.main.ScreenPointToRay(touch.position);
+
+            RaycastHit hitInfo;
+
+            if (Physics.Raycast (myRay, out hitInfo, 100, whatCanBePressedOn))
+            {
+                myAgent.SetDestination(hitInfo.point);
+            }
+        }
+    }
+}
